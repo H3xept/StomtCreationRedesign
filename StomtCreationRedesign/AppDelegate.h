3df55a33c1482d6c3a9abb343be96a03c5f2c8ad
H3xept <h3xept@gmail.com>
2016-11-17 20:03:02 +0100

    Initial Commit

---
+++ StomtCreationRedesign/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StomtCreationRedesign
+//
+//  Created by Leonardo Cascianelli on 17/11/2016.
+//  Copyright © 2016 Leonardo Cascianelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StomtCreationRedesign/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StomtCreationRedesign
+//
+//  Created by Leonardo Cascianelli on 17/11/2016.
+//  Copyright © 2016 Leonardo Cascianelli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
